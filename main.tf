91d02031cca217527d66c79e6f0fab53a86a3735
白川遥平 <youhei@youhei.local>
2020-04-15 13:34:43 +0900

    change_provider_version

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "gke-cluster"
+  name     = var.cluster_name
   location = var.location
 
   remove_default_node_pool = true
--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  version = "3.9.0"
+  version = "3.17.0"
   project = var.project
   region  = var.location
 }
