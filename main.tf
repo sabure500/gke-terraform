0932f321caa95ca99123186b3bc2af3ae8d444e3
sabure500 <shirakawayouhei.tec@gmail.com>
2020-11-07 21:36:55 +0900

    tagを追加

--- main.tf
+++ main.tf
@@ -7,9 +7,6 @@ resource "google_container_cluster" "primary" {
 
   network = var.network
 
-  # min_master_version = var.min_master_version
-  # node_version       = var.node_version
-
   monitoring_service = "none"
   logging_service    = "none"
 
@@ -22,27 +19,6 @@ resource "google_container_cluster" "primary" {
     }
   }
 }
-
-# f1-microのサイズはメモリ不足によりGKEのNODEが作成できなくなっていたため、コメントアウト
-# resource "google_container_node_pool" "primary_nodes" {
-#   name       = "${var.cluster_name}-node-pool"
-#   location   = var.location
-#   cluster    = google_container_cluster.primary.name
-#   node_count = 1
-
-#   management {
-#     auto_repair = true
-#   }
-
-#   node_config {
-#     machine_type = var.machine_type
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/devstorage.read_only",
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring"
-#     ]
-#   }
-# }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "${var.cluster_name}-preemptible-node-pool"
@@ -62,5 +38,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
+    tags = ["${var.cluster_name}-node"]
   }
 }
